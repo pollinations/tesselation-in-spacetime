a6b1a7ca444a4100cba7f85eacc19ffca345fa51
eni-lorac <39300569+eni-lorac@users.noreply.github.com>
2021-06-12 13:10:58 +0200

    cores e regra preferidos

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
     const [timeToNext, setTimeToNext] = useState("---");
     const [desiredIteration, setDesiredIteration] = useState(0);
 
-    const [rule, setRule] = useState("1234562/23456715161718");
+    const [rule, setRule] = useState("23456715161719/345671516171819");
 
     const {cell, iterate, iteration} = useCelularAutomata({size, rule});
 
